2562e3303f016020c4f15d5c68f283443fd86d1b
RestingaPUC <carneiroalberto@yahoo.com.br>
2023-02-13 14:57:01 -0300

    3

--- CSS Pages/Alexa/js/main.js
+++ CSS Pages/Alexa/js/main.js
@@ -1,16 +1,30 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
 
-
-/*===== MENU SHOW =====*/
+/*===== SHOW MENU =====*/
 /* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
-
-/*===== MENU HIDDEN =====*/
+/*===== HIDE MENU =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
-
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction() {
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(e => e.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
